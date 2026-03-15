c1b420594410616031869dcd1ab984c19fa91127
DaviP-Conceicao <davipaulino7@hotmail.com>
2026-03-15 10:10:33 -0300

    Adicionando projeto Lixo Zero Dashboard

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const links = document.querySelectorAll('.nav-links li');
+
+   
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+    });
+
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.classList.remove('open');
+        });
+    });
+});
