0766758b440d883b769c0cae675fbdb61a7b5312
DaviP-Conceicao <davipaulino7@hotmail.com>
2026-05-03 12:40:09 -0300

    feat: Adicionando mais projetos e atualizando o portfólio

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
     const navLinks = document.querySelectorAll('.nav-link');
     const menuToggle = document.getElementById('navbarNav');
-    const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false});
+    
+    if (menuToggle) {
+        const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle: false});
 
-    navLinks.forEach((l) => {
-        l.addEventListener('click', () => {
-            if (window.innerWidth < 992) {
-                bsCollapse.toggle();
-            }
+        navLinks.forEach((l) => {
+            l.addEventListener('click', () => {
+                if (window.innerWidth < 992) {
+                    bsCollapse.toggle();
+                }
+            });
         });
-    });
+    }
 });
